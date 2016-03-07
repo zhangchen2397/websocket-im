fc7386b7cdc41534783a37f48af7db1923be0b87
zhangchen <zhangchen2397@126.com>
2016-03-07 09:17:50 +0800

    up by zc

--- index.js
+++ index.js
@@ -15,9 +15,7 @@ var numUsers = 0;
 io.on('connection', function(socket) {
     var addedUser = false;
 
-    // when the client emits 'new message', this listens and executes
     socket.on('new message', function(data) {
-        // we tell the client to execute 'new message'
         socket.broadcast.emit('new message', {
             username: socket.username,
             message: data,
@@ -25,11 +23,9 @@ io.on('connection', function(socket) {
         });
     });
 
-    // when the client emits 'add user', this listens and executes
     socket.on('add user', function(username) {
         if (addedUser) return;
 
-        // we store the username in the socket session for this client
         ++numUsers;
         socket.username = username;
         socket.avatar = '/image/avatar' + numUsers + '.jpg';
@@ -41,33 +37,28 @@ io.on('connection', function(socket) {
             username: username,
             avatar: socket.avatar
         });
-        // echo globally (all clients) that a person has connected
         socket.broadcast.emit('user joined', {
             username: socket.username,
             numUsers: numUsers
         });
     });
 
-    // when the client emits 'typing', we broadcast it to others
     socket.on('typing', function() {
         socket.broadcast.emit('typing', {
             username: socket.username
         });
     });
 
-    // when the client emits 'stop typing', we broadcast it to others
     socket.on('stop typing', function() {
         socket.broadcast.emit('stop typing', {
             username: socket.username
         });
     });
 
-    // when the user disconnects.. perform this
     socket.on('disconnect', function() {
         if (addedUser) {
             --numUsers;
 
-            // echo globally that this client has left
             socket.broadcast.emit('user left', {
                 username: socket.username,
                 numUsers: numUsers
